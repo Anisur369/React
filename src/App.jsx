0131c5a501f6c7a07b861d1cdde918d3617c9ec2
Anisur369 <anisurrahman57136@gmail.com>
2023-07-27 08:22:48 +0600

    new date

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
-function App() {
+import React from "react"
+import { ReactDOM } from "react"
+
+class App extends React.Component{
+    render(){
+        return (
+            <h1 className="heading">
+                <span className="text">Hello {new Date().toLocaleTimeString(this.props.locale)}</span>
+            </h1>
+        )
+    }
 }
 
 export default App
