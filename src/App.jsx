a328d94a80b28f61b5e0cb7ed0175271a8f1f0c6
Anisur369 <anisurrahman57136@gmail.com>
2023-08-24 05:26:55 +0600

    composition

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import React from "react"
 import Emoji from "./components/composition/Emoji";
+import Bracket from "./components/composition/Bracket";
 import Text from "./components/composition/Text";
 
 
 function App(){
     return (
+        <div>
         <Emoji>
-            {({addEmoji})=>{return <Text addEmoji={addEmoji}/>}}
+            {({addEmoji})=>
+            <Bracket>
+                {({addBracket})=><Text addEmoji={addEmoji} addBracket={addBracket}/>}
+            </Bracket>}
         </Emoji>
+        <br/>
+        <br/>
+
+        <Emoji>
+            {({addBracket})=><Text addBracket={addBracket}/>}
+        </Emoji>
+
+
+        </div>
     )
 }
 
--- src/components/composition/Text.jsx
+++ src/components/composition/Text.jsx
@@ -1,8 +1,14 @@
-export default function Text({addEmoji}){
+export default function Text({addEmoji,addBracket}){
     let text='I am JavaScript Programming Language';
+    if(addEmoji){
+                text=addEmoji(text,'💖')
+            }
+            if(addBracket){
+                text=addBracket(text,'💖')
+            }
     return (
         <div>
-            {addEmoji?addEmoji(text,'💖'):text}
+            {text}
         </div>
     )
 }
