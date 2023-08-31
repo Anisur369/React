1da6638f50bef174dd2f65ef178629bb5c46b9e6
Anisur369 <anisurrahman57136@gmail.com>
2023-08-31 23:52:56 +0600

    User & Gest

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import ClickCounter from "./components/ClickCounter";
+import ClickCounter from "./components/HOC/ClickCounter";
+import HoverCounter from "./components/HOC/HoverCounter";
+import User from "./components/User";
 
 function App(){
     return (
         <div className="app">
             <ClickCounter/>
+            <HoverCounter/>
+            <User name={(isLoggedin)=>{return isLoggedin?"Sumit":"Geust" }}/>
         </div>
     )
 }
