ffdd863ea89d351cb142e67d70e4bbf82f630e38
Anisur369 <anisurrahman57136@gmail.com>
2023-12-29 17:58:34 +0600

    commit

--- src/components/HOC/Content.jsx
+++ src/components/HOC/Content.jsx
@@ -2,22 +2,21 @@ import React from "react";
 import Counter from "./Counter";
 import HoverCounter from "./HoverCounter";
 import themeContext from "../../Contexts/themeContext";
+import { useContext } from "react";
 
-export default class Content extends React.Component{
-    render(){
-        console.log(this.context)
-        const {theme,switchTheme}=this.context;
-        return(
-            <div>
-                <h1>This is a HoverCounter</h1>
-                <Counter>
-                    {(count,incrementCount)=>{
-                        return ( 
-                        <HoverCounter count={count} incrementCount={incrementCount} theme={theme} switchTheme={switchTheme}/>)
-                    }}
-                </Counter>
-            </div>
-        )
-    }
+export default function Content(){
+    const context=useContext(themeContext);
+    const {theme,switchTheme}=context;
+    return(
+        <div>
+            <h1>This is a HoverCounter</h1>
+            <Counter>
+                {(count,incrementCount)=>{
+                    return (
+                        <HoverCounter count={count} incrementCount={incrementCount} theme={theme} switchTheme={switchTheme}/>
+                    )
+                }}
+            </Counter>
+        </div>
+    )
 }
-Content.contextType=themeContext;
