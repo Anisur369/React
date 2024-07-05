0c6a2838e534899ba125f3b462fa7a0e3f840231
Anisur369 <anisurrahman57136@gmail.com>
2024-07-05 14:53:24 +0600

    all updataet

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
 import React from "react"
-import Counter from "./components/HOC/Counter";
-import ClickCounter from "./components/HOC/ClickCounter";
-import Section from "./components/HOC/Section";
-import themeContext from "./Contexts/themeContext";
+import Todo from "./components/Todo";
 
-
-class App extends React.Component{
-    state={
-        theme:'dark',
-        switchTheme:()=>{
-            this.setState(({theme})=>{
-                if(theme=='dark'){
-                    return {
-                        theme:'light'
-                    }
-                }else{
-                    return {
-                        theme:'dark'
-                    }
-                }
-            })
-        }
-    }
-    
-    render(){
-        console.log('App theme')
-        const {theme,switchTheme}=this.state;
-        return (
-            <div>
-                <Counter>
-                    {(count,incrementCount)=>(<ClickCounter count={count} incrementCount={incrementCount}/>)}
-                </Counter>
-                <themeContext.Provider value={{theme,switchTheme:switchTheme}}>
-                    <Section/>
-                </themeContext.Provider>
-            </div>
-        )
-    }
+function App(){
+    return (<div>
+        <Todo/>
+    </div>)
 }
-
-export default App
+export default App;
--- src/components/HOC/ClickCounter.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function ClickCounter({count,incrementCount}){
-    return (
-        <button onClick={incrementCount}>Click {count} me</button>
-    )
-}
