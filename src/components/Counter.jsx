58dabfbff9b18036cc463f270389ffb6251a5aea
Anisur369 <anisurrahman57136@gmail.com>
2024-07-31 05:29:39 +0600

    com

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,19 +1,12 @@
 import { useReducer } from "react";
 
-const initialState={
-    counter:5,
-    counter2:0
-};
+const initialState=0;
 const reducer=(state,action)=>{
-    switch (action.type){
+    switch (action){
         case 'increment':
-            return {...state,counter:state.counter+action.value};
+            return state+1;
         case 'decrement':
-            return {...state,counter:state.counter-action.value};
-        case 'increment2':
-            return {...state,counter2:state.counter2+action.value};
-        case 'decrement2':
-            return {...state,counter2:state.counter2-action.value};
+            return state-1;
         default:
             return state;
     }
@@ -22,16 +15,9 @@ function Counter(){
     const [count,dispatch]=useReducer(reducer,initialState)
     return (
         <div>
-            <div>
-            <div>Count - {count.counter}</div>
-            <button type="button" onClick={()=>dispatch({type:'increment',value:1})}>Increment by 1</button>
-            <button type="button" onClick={()=>dispatch({type:'decrement',value:1})}>Decrement by -1</button>
-            </div>
-            <div>
-            <div>Count2 - {count.counter2}</div>
-            <button type="button" onClick={()=>dispatch({type:'increment2',value:1})}>Increment by 1</button>
-            <button type="button" onClick={()=>dispatch({type:'decrement2',value:1})}>Decrement by -1</button>
-            </div>
+            <div>Count - {count}</div>
+            <button type="button" onClick={()=>dispatch('increment')}>Increment</button>
+            <button type="button" onClick={()=>dispatch('decrement')}>Decrement</button>
         </div>
     )
 }
