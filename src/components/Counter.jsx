0ba1d793408088bc86c282d08bc358c752d3bd1d
Anisur369 <anisurrahman57136@gmail.com>
2024-08-10 03:03:02 +0600

    commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,26 +1,25 @@
-import { createContext, useReducer } from "react";
-import ComponentC from "./ComponentC";
+const {useReducer}=React()
 
-export const newContext=createContext();
-const initialState=0;
-const reducer=(state,action)=>{
-    switch (action){
-        case 'increment':
-            return state+1;
-        case 'decrement':
-            return state-1;
+const countValue=0;
+const redue=(state,action)=>{
+    switch (action) {
+        case increment:
+            return state+1
+        case decrement:
+            return state-1
+            break;    
         default:
             return state;
     }
 }
+
 function Counter(){
-    const [count,dispatch]=useReducer(reducer,initialState)
+    const [count,dispatch]=useReducer(redue,countValue)
     return (
         <div>
-            <newContext.Provider value={{count,dPath:dispatch}}>
-                <ComponentC/>
-            </newContext.Provider>
-            
+            <p>count- {count}</p>
+            <button onClick={()=>dispatch('increment')}>increment</button>
+            <button onClick={()=>dispatch('decrement')}>decrement</button>
         </div>
     )
 }
